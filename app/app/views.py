6f3322cc46a8911773f3f551c6ceae2299430857
angrun <angrun@ttu.ee>
2022-07-03 14:35:20 +0300

    test

--- app/app/views.py
+++ app/app/views.py
@@ -8,7 +8,7 @@ def get_prediction(request, image_url):
     print(image_url)
     if request.method == 'GET':
         response_data = {'probabilities': [0.91004425, 0.0899557]}
-        image_url = image_url.replace("-", "/")
+        image_url = image_url.replace("-", "/").replace("/home/docker/ajapaik/", "/home/anna/ajapaik-web/" )
         print(image_url)
         response = TrainigService().predict(image_url)
         print(response)
