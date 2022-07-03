089fcb8594621fd0a39ef33709ed1a6efd7de204
angrun <angrun@ttu.ee>
2022-07-03 10:23:54 +0300

    test

--- app/app/algorithms/transfer_learning.py
+++ app/app/algorithms/transfer_learning.py
@@ -33,7 +33,7 @@ def get_data(data_dir):
 def get_model():
 
     train = get_data(
-        '/Users/annagrund/PycharmProjects/ajapaik-learning/app/app/algorithms/interior_exterior/input/train')
+        '/ajapaik-learning/app/app/algorithms/interior_exterior/input/train')
     test = get_data('/Users/annagrund/PycharmProjects/ajapaik-learning/app/app/algorithms/interior_exterior/input/test')
 
 
