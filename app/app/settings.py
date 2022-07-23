b84cea4f760f13ff472d756559085f50963e585e
angrun <angrun@ttu.ee>
2022-07-23 15:28:13 +0300

    test

--- app/app/settings.py
+++ app/app/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['staging.ajapaik.ee', 'anna.ajapaik.ee', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['https://anna.ajapaik.ee']
+CSRF_TRUSTED_ORIGINS = ['https://anna.ajapaik.ee', 'http://localhost:8000']
 
 # Application definition
 
